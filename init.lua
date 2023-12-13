45954b1922e621806ccfeb302f829e0ff23218fd
George S. Mulbah <gsmulbah2500@gmail.com>
2023-12-13 11:34:28 +0000

    add nvim config

---
+++ init.lua
@@ -0,0 +1,14 @@
+require("plugins")
+require("remap")
+require("set")
+
+
+local augroup = vim.api.nvim_create_augroup
+local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
